c902101d7c5efa51dbcaf479c3e3fc90b60000c1
Mike Gouline <mgouline@assemblypayments.com>
2018-05-10 12:46:50 +1000

    Replace Charsets constant from Grizzly to standard Commons

--- client/src/main/java/com/assemblypayments/spi/model/BillStatusResponse.java
+++ client/src/main/java/com/assemblypayments/spi/model/BillStatusResponse.java
@@ -2,9 +2,9 @@ package com.assemblypayments.spi.model;
 
 import com.assemblypayments.spi.util.Events;
 import com.google.gson.Gson;
+import org.apache.commons.codec.Charsets;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang.StringUtils;
-import org.glassfish.grizzly.utils.Charsets;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -122,7 +122,7 @@ public class BillStatusResponse {
         if (billData == null || StringUtils.isWhitespace(billData)) return new ArrayList<PaymentHistoryEntry>();
 
         final byte[] bdArray = Base64.decodeBase64(billData);
-        final String bdStr = new String(bdArray, Charsets.UTF8_CHARSET);
+        final String bdStr = new String(bdArray, Charsets.UTF_8);
         return GSON.fromJson(bdStr, PaymentHistoryEntry.ListType.class);
     }
 
@@ -130,7 +130,7 @@ public class BillStatusResponse {
         if (ph.isEmpty()) return "";
 
         final String bphStr = GSON.toJson(ph);
-        return Base64.encodeBase64String(bphStr.getBytes(Charsets.UTF8_CHARSET));
+        return Base64.encodeBase64String(bphStr.getBytes(Charsets.UTF_8));
     }
 
     public Message toMessage(String messageId) {
