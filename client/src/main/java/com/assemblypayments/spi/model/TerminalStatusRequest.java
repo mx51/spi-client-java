7a13c24caf9edea07b5213ed854a1b90a7fce64c
Metin Avci <mavci@assemblypayments.com>
2018-10-09 16:08:40 +1100

    Upgrading to v2.4.0
    SCLJ-9 Reporting Battery Level Changing
    SCLJ-10 Reporting Terminal Status
    SCLJ-11 Free form printing is not working

---
+++ client/src/main/java/com/assemblypayments/spi/model/TerminalStatusRequest.java
@@ -0,0 +1,19 @@
+package com.assemblypayments.spi.model;
+
+import com.assemblypayments.spi.util.Events;
+import com.assemblypayments.spi.util.RequestIdHelper;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TerminalStatusRequest {
+
+    @NotNull
+    public Message toMessage() {
+        final Map<String, Object> data = new HashMap<String, Object>();
+
+        return new Message(RequestIdHelper.id("trmnl"), Events.TERMINAL_STATUS_REQUEST, data, true);
+    }
+
+}
