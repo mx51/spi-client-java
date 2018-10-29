8e837a1e6c898bc27327bd95b51eb30505dc5664
Metin Avci <mavci@assemblypayments.com>
2018-10-29 15:38:14 +1100

    Add TransacitonOption null control

--- client/src/main/java/com/assemblypayments/spi/model/AbstractChargeRequest.java
+++ client/src/main/java/com/assemblypayments/spi/model/AbstractChargeRequest.java
@@ -8,7 +8,7 @@ public abstract class AbstractChargeRequest implements Message.Compatible {
     private final String posRefId;
 
     private SpiConfig config = new SpiConfig();
-    
+
     private TransactionOptions options = new TransactionOptions();
 
     protected AbstractChargeRequest(String posRefId) {
@@ -26,13 +26,17 @@ public abstract class AbstractChargeRequest implements Message.Compatible {
     public void setOptions(TransactionOptions options) {
         this.options = options;
     }
-    
+
     protected Message toMessage(String id, String eventName, Map<String, Object> data, boolean needsEncryption) {
         final Map<String, Object> baseData = new HashMap<String, Object>();
         baseData.put("pos_ref_id", getPosRefId());
         baseData.putAll(data);
         config.addReceiptConfig(baseData);
-        options.addOptions(baseData);
+
+        if (options != null) {
+            options.addOptions(baseData);
+        }
+
         return new Message(id, eventName, baseData, needsEncryption);
     }
 
